bf0613dfcfc79a32b0363dce6e8d781cc66d2b35
Sebastian Hans <Hans.Sebastian@swm.de>
2024-09-16 09:06:58 +0200

    add folding

--- after/ftplugin/time-tracking.lua
+++ after/ftplugin/time-tracking.lua
@@ -2,3 +2,6 @@ local bufno = vim.api.nvim_get_current_buf()
 vim.keymap.set({'i', 'n'}, '<S-Up>',   '<Plug>(TimeTrackingInc)',  {buffer = bufno})
 vim.keymap.set({'i', 'n'}, '<S-Down>', '<Plug>(TimeTrackingDec)',  {buffer = bufno})
 vim.keymap.set({'i', 'n'}, '<C-s>',    '<Plug>(TimeTrackingDone)', {buffer = bufno})
+
+vim.opt_local.foldmethod = 'indent'
+vim.opt_local.shiftwidth = 2
